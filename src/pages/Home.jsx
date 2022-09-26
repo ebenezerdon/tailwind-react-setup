8d5644a589c8d43dd5cd4e8a276a81b47a43a7d3
eliscodavid@gmail.com <eliscodavid@gmail.com>
2022-09-26 12:26:25 +0100

    upload profile pic

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,7 +24,7 @@ return (
         </div>
     </div>
     <div class="flex flex-col items-center pb-10">
-        <img class="mb-3 w-24 h-24 rounded-full shadow-lg" src="/docs/images/people/profile-picture-3.jpg" alt="Bonnie image"/>
+        <img class="mb-3 w-24 h-24 rounded-full shadow-lg" src="/pages/vivian.png" alt="Vivian Afolabi"/>
         <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Bonnie Green</h5>
         <span class="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
         <div class="flex mt-4 space-x-3 md:mt-6">
