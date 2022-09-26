20995d3f0cb8dfed1d31b7ec21cdb72858c0a268
eliscodavid@gmail.com <eliscodavid@gmail.com>
2022-09-26 12:18:04 +0100

    edit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ return (
             <span class="sr-only">Open dropdown</span>
             <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z"></path></svg>
         </button>
-        <!-- Dropdown menu -->
+        
         <div id="dropdown" class="hidden z-10 w-44 text-base list-none bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700">
             <ul class="py-1" aria-labelledby="dropdownButton">
             <li>
